e6127a6f28fb6af50de2f91f32ff816549099a0f
sawmon <sawmonhan@acedatasystems.com>
2023-03-01 04:01:25 +0100

    fixed path to htmlwebpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('./node_modules/html-webpack-plugin');
+
 module.exports = {
   mode: 'development',
   module: {
