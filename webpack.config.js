19f336715c573789efcee7b399c7acf02f3ca736
sawmon <sawmonhan@acedatasystems.com>
2023-03-01 03:53:26 +0100

    used htmlplugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -11,8 +12,15 @@ module.exports = {
     ],
   },
   entry: './src/index.js',
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
 };
