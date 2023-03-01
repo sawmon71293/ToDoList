7c7f807abbbcad24de85a3457423812fe22d8554
sawmon <sawmonhan@acedatasystems.com>
2023-03-01 07:36:33 +0100

    refactored code for add and remove functions

--- src/index.js
+++ src/index.js
@@ -1,21 +1,8 @@
 import './index.css';
-
-const tasks = [
-  {
-    description: 'to cook',
-    completed: true,
-    index: 0,
-  },
-  {
-    description: 'to make coffee',
-    completed: false,
-    index: 1,
-  },
-];
-
-const loadList = (tasks) => {
+const toDoList = document.getElementById('todolist');
+const loadTask = (tasks) => {
   tasks.sort((a, b) => a.index - b.index);
-  const toDoList = document.getElementById('todolist');
+
   toDoList.innerHTML = '';
   tasks.forEach((task) => {
     const template = ` <li class= "border-bottom border-opacity-10 p-3 d-flex justify-content-between">
@@ -23,12 +10,67 @@ const loadList = (tasks) => {
                           <input type="checkbox" class="me-2 mt-2" />
                           ${task.description}
                           </div>
-                          <div><div class="ellipsis"><button class="btn"><i class="fa-solid fa-ellipsis-vertical"></i></button></div>
-                          <div class="trash"></i><i class="fa-solid fa-trash-can"></i></div>
+                          <div>
+                          <div class="ellipsis">
+                            <button class="btn btn-ellipsis" data-index="${task.index}">
+                              <i class="fa-solid fa-ellipsis-vertical"></i>
+                            </button>
                           </div>
+                          <div class="hidden">
+                            <button class="btn btn-trash" data-index="${task.index}">
+                              <i class="fa-solid fa-trash-can"></i>
+                            </button>
+                          </div>
+                        </div>
                        </li>`;
     toDoList.innerHTML += template;
   });
 };
 
-loadList(tasks);
+const addTask = () => {
+  const add = document.getElementById('add').value;
+  const tasks = JSON.parse(localStorage.getItem('tasks') || JSON.stringify([]));
+
+  const newTask = {
+    description: add,
+    completed: false,
+    index: tasks.length + 1,
+  };
+
+  tasks.push(newTask);
+  loadTask(tasks);
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+};
+
+const removeTask = (value) => {
+  let tasks = JSON.parse(localStorage.getItem('tasks'));
+  let filtered = tasks.filter((task) => task.index !== parseInt(value));
+  filtered.forEach((task, i) => {
+    task.index = i + 1;
+  });
+  localStorage.setItem('tasks', JSON.stringify(filtered));
+  loadTask(filtered);
+};
+
+const input = document.getElementById('add');
+input.addEventListener('keyup', function (event) {
+  event.preventDefault();
+  if (event.key === 'Enter') {
+    addTask();
+  }
+});
+
+const tasks = JSON.parse(localStorage.getItem('tasks'));
+if (tasks) loadTask(tasks);
+
+toDoList.addEventListener('click', (e) => {
+  if (e.target.closest('.btn-ellipsis')) {
+    const ellipsisBtn = e.target.closest('.btn-ellipsis');
+    const trashBtn = ellipsisBtn.closest('.ellipsis').nextElementSibling;
+    ellipsisBtn.classList.add('hidden');
+    trashBtn.classList.remove('hidden');
+  } else if (e.target.closest('.btn-trash')) {
+    const trashBtn = e.target.closest('.btn-trash');
+    removeTask(trashBtn.dataset.index);
+  }
+});
