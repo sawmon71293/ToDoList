c367c734c88b5122a7c95f794405d4143d575582
sawmon <sawmonhan@acedatasystems.com>
2023-03-02 04:18:18 +0100

    fixed linters errors

--- src/checkbox.js
+++ src/checkbox.js
@@ -1,10 +1,10 @@
 const checkBoxes = (event) => {
-  const tasks = JSON.parse(localStorage.getItem('tasks'));
-   if (event.target.type === 'checkbox') {
-     const index = parseInt(event.target.dataset.index, 10);
-     const task = tasks.find((task) => task.index === index);
-     task.completed = event.target.checked;
-     localStorage.setItem('tasks', JSON.stringify(tasks));
+    const tasks = JSON.parse(localStorage.getItem('tasks'));
+    if (event.target.type === 'checkbox') {
+      const index = parseInt(event.target.dataset.index, 10);
+      const task = tasks.find((task) => task.index === index);
+      task.completed = event.target.checked;
+      localStorage.setItem('tasks', JSON.stringify(tasks));
     }
 };
 
--- src/index.js
+++ src/index.js
@@ -101,7 +101,7 @@ clearBtn.addEventListener('click', (e) => {
   e.preventDefault();
   const tasks = JSON.parse(localStorage.getItem('tasks'));
   const filtered = tasks.filter((task) => !task.completed);
-  filtered.forEach((task, i) => { task.index = i + 1 });
+  filtered.forEach((task, i) => { task.index = i + 1; });
   localStorage.setItem('tasks', JSON.stringify(filtered));
   loadTask(filtered);
 });
